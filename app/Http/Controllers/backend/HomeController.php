27600625f95446c34991775af5a556cad1595a1b
berflower <anakmbarep999@gmail.com>
2021-02-21 10:02:50 +0700

    update kecil front end

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
     //=================================================================
     public function index()
     {
-        return view('backend.dashboard.index');
+        return view('frontend.index');
     }
 
     //==================================================================
---
+++ resources/views/frontend/base/css.blade.php
@@ -0,0 +1,25 @@
+     <!-- Place favicon.ico in the root directory -->
+     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+     <link rel="apple-touch-icon" href="apple-touch-icon.png">
+     
+ 
+     <!-- All css files are included here. -->
+     <!-- Bootstrap fremwork main css -->
+     <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
+     <!-- Owl Carousel main css -->
+     <link rel="stylesheet" href="{{asset('frontend/css/owl.carousel.min.css')}}">
+     <link rel="stylesheet" href="{{asset('frontend/css/owl.theme.default.min.css')}}">
+     <!-- This core.css file contents all plugings css file. -->
+     <link rel="stylesheet" href="{{asset('frontend/css/core.css')}}">
+     <!-- Theme shortcodes/elements style -->
+     <link rel="stylesheet" href="{{asset('frontend/css/shortcode/shortcodes.css')}}">
+     <!-- Theme main style -->
+     <link rel="stylesheet" href="{{asset('frontend/style.css')}}">
+     <!-- Responsive css -->
+     <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
+     <!-- User style -->
+     <link rel="stylesheet" href="{{asset('frontend/css/custom.css')}}">
+ 
+ 
+     <!-- Modernizr JS -->
+     <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
